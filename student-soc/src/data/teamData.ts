511bd44a42d39b097caf5119ef016cbcfca81642
tommy phao <thomasphao04@gmail.com>
2024-09-03 13:59:22 -0700

    current update -missing faculty

--- student-soc/src/data/teamData.ts
+++ student-soc/src/data/teamData.ts
@@ -3,20 +3,39 @@ const teamData = [
       name: 'Joseph Shin',
       year: "MS",
       program: 'Information Security',
-      description: 'Student SOC Lead',
+      description: 'Director of SOC',
       picture: 'https://media.licdn.com/dms/image/v2/D5603AQHC0XTbQrsRMw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1711247876014?e=1730332800&v=beta&t=t3slWU1QLtzvuvgnWSQEYxQgnjvxExZdE_oSy9MTLSI',
       media: {
         linkedin: 'https://www.linkedin.com/in/hsshin95/',
-        github: 'https://github.com/johndoe',
-        twitter: 'https://twitter.com/johndoe',
-        instagram: 'https://instagram.com/johndoe',
+        github: 'https://github.com/Sh0bra',
       },
     },
     {
-        name: 'Joseph Shin',
+        name: 'Drake Huffman',
+        year: "Junior",
+        program: 'Computer Information Systems',
+        description: 'Security Engineer Lead',
+        picture: 'https://media.licdn.com/dms/image/v2/C5603AQF4-tz8yjZ1kg/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1658646293399?e=1730937600&v=beta&t=Hvd5iPEe6IG57nMKBw3zmwgM_vnStMPjIqt9lXY37EA',
+        media: {
+          linkedin: 'https://www.linkedin.com/in/drakehuffman/',
+        },
+      },
+      {
+        name: 'Armin Erika Polanco',
         year: "MS",
         program: 'Information Security',
-        description: 'Student SOC Lead',
+        description: 'Vice President of Operations',
+        picture: 'https://media.licdn.com/dms/image/v2/D5603AQGCXW5O03tOvQ/profile-displayphoto-shrink_100_100/profile-displayphoto-shrink_100_100/0/1695465903236?e=1730937600&v=beta&t=tCIdXPrcpSJmYVE_15rQmFn0Uyc65g34B1JxcI_MWzs',
+        media: {
+          linkedin: 'https://www.linkedin.com/in/arminerika//',
+          github: 'https://github.com/arminerika',
+        },
+      },
+      {
+        name: 'Ari Torczon',
+        year: "MS",
+        program: 'Information Security',
+        description: 'Vice President of Finance',
         picture: 'https://media.licdn.com/dms/image/v2/D5603AQHC0XTbQrsRMw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1711247876014?e=1730332800&v=beta&t=t3slWU1QLtzvuvgnWSQEYxQgnjvxExZdE_oSy9MTLSI',
         media: {
           linkedin: 'https://www.linkedin.com/in/hsshin95/',
@@ -26,10 +45,20 @@ const teamData = [
         },
       },
       {
-        name: 'Joseph Shin',
-        year: "MS",
-        program: 'Information Security',
-        description: 'Student SOC Lead',
+        name: 'Jake Ludlow',
+        year: "Senior",
+        program: 'Computer Science',
+        description: 'Scheduler',
+        picture: 'https://media.licdn.com/dms/image/v2/D5635AQHK7If77peSvg/profile-framedphoto-shrink_200_200/profile-framedphoto-shrink_200_200/0/1689130406817?e=1726002000&v=beta&t=t-uBskgt7KzcR6oPWTrWwRpWufNpFizJggK7tyjW8Jk',
+        media: {
+          linkedin: 'https://www.linkedin.com/in/jakeludlow/',
+        },
+      },
+      {
+        name: 'Eric Heng',
+        year: "Junior",
+        program: 'Computer Information Systems',
+        description: 'Vice President of Outreach',
         picture: 'https://media.licdn.com/dms/image/v2/D5603AQHC0XTbQrsRMw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1711247876014?e=1730332800&v=beta&t=t3slWU1QLtzvuvgnWSQEYxQgnjvxExZdE_oSy9MTLSI',
         media: {
           linkedin: 'https://www.linkedin.com/in/hsshin95/',
@@ -39,10 +68,10 @@ const teamData = [
         },
       },
       {
-        name: 'Joseph Shin',
-        year: "MS",
-        program: 'Information Security',
-        description: 'Student SOC Lead',
+        name: 'Thomas Phao',
+        year: "Junior",
+        program: 'Computer Information Systems',
+        description: 'Webmaster',
         picture: 'https://media.licdn.com/dms/image/v2/D5603AQHC0XTbQrsRMw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1711247876014?e=1730332800&v=beta&t=t3slWU1QLtzvuvgnWSQEYxQgnjvxExZdE_oSy9MTLSI',
         media: {
           linkedin: 'https://www.linkedin.com/in/hsshin95/',
@@ -51,33 +80,6 @@ const teamData = [
           instagram: 'https://instagram.com/johndoe',
         },
       },
-      {
-        name: 'Joseph Shin',
-        year: "MS",
-        program: 'Information Security',
-        description: 'Student SOC Lead',
-        picture: 'https://media.licdn.com/dms/image/v2/D5603AQHC0XTbQrsRMw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1711247876014?e=1730332800&v=beta&t=t3slWU1QLtzvuvgnWSQEYxQgnjvxExZdE_oSy9MTLSI',
-        media: {
-          linkedin: 'https://www.linkedin.com/in/hsshin95/',
-          github: 'https://github.com/johndoe',
-          twitter: 'https://twitter.com/johndoe',
-          instagram: 'https://instagram.com/johndoe',
-        },
-      },
-      {
-        name: 'Joseph Shin',
-        year: "MS",
-        program: 'Information Security',
-        description: 'Student SOC Lead',
-        picture: 'https://media.licdn.com/dms/image/v2/D5603AQHC0XTbQrsRMw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1711247876014?e=1730332800&v=beta&t=t3slWU1QLtzvuvgnWSQEYxQgnjvxExZdE_oSy9MTLSI',
-        media: {
-          linkedin: 'https://www.linkedin.com/in/hsshin95/',
-          github: 'https://github.com/johndoe',
-          twitter: 'https://twitter.com/johndoe',
-          instagram: 'https://instagram.com/johndoe',
-        },
-      },
-    // Add more team members here
   ];
   
   export default teamData;
