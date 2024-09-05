04c5fa2661bb7475e1db63be0c2f64c7c80188d8
tommy phao <thomasphao04@gmail.com>
2024-09-05 14:33:43 -0700

    typo

--- student-soc/next.config.mjs
+++ student-soc/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['wiki.cppsoc,xyz', 'media.licdn.com', 'encrypted-tbn0.gstatic.com', 'cppsoc.xyz', 'github.com', 'raw.githubusercontent.com'],
+        domains: ['wiki.cppsoc.xyz', 'media.licdn.com', 'encrypted-tbn0.gstatic.com', 'cppsoc.xyz', 'github.com', 'raw.githubusercontent.com'],
       }
 };
 
