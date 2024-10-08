0469f6f7da9d7f06e3eeb132372150eb4ec812cd
tommy phao <thomasphao04@gmail.com>
2024-10-08 15:09:19 -0700

    wbi4yfehnjockpoqefdm;

--- student-soc/src/components/Navbar.tsx
+++ student-soc/src/components/Navbar.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import logo from '/logo3.png';
+import logo from 'logo3.png';
 
 const Navbar: React.FC = () => {
   return (
