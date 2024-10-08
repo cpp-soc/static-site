609a0bbb35996a1e6ffddad84218482532b0d090
tommy phao <thomasphao04@gmail.com>
2024-10-08 15:12:40 -0700

    fyvguhiuj

--- student-soc/next.config.mjs
+++ student-soc/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   images: {
       loader: 'default',
-      path: '/assets/**',
+      path: '/assets/',
       domains: ['https://wiki.cppsoc.xyz', 'media.licdn.com', 'encrypted-tbn0.gstatic.com', 'cppsoc.xyz', 'github.com', 'raw.githubusercontent.com'],
   }
 };
