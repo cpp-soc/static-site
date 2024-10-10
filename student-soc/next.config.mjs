6d2246da7b39b59a3927e63c30e82e13170cf6e2
tommy phao <thomasphao04@gmail.com>
2024-10-09 19:32:25 -0700

    removed all image loader paramas

--- student-soc/next.config.mjs
+++ student-soc/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-      loader: 'default',
-      path: '',
       domains: ['https://wiki.cppsoc.xyz', 'media.licdn.com', 'encrypted-tbn0.gstatic.com', 'cppsoc.xyz', 'github.com', 'raw.githubusercontent.com'],
   }
 };
