de6ca9586685d805ef1912cf262d0352a9fe90d8
tommy phao <thomasphao04@gmail.com>
2024-10-09 19:41:36 -0700

    moved assets to public, testing with joe image

--- student-soc/src/data/teamData.ts
+++ student-soc/src/data/teamData.ts
@@ -4,7 +4,7 @@ const teamData = [
       year: "MS",
       program: 'Information Security',
       description: 'Director of SOC',
-      picture: 'https://media.licdn.com/dms/image/v2/D5603AQHC0XTbQrsRMw/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1711247876014?e=1730937600&v=beta&t=-mVdprt7cN95bZRK5Gfw_vinnMdwgfyA4585MfVCPvg',
+      picture: 'https://cppsoc.xyz/assets/2024-team/director.jpg',
       media: {
         linkedin: 'https://www.linkedin.com/in/hsshin95/',
         github: 'https://github.com/Sh0bra',
