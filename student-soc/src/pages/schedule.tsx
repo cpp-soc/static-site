b923fb9e981c7a4faf254bfff2f226ca95e3dd34
tommy phao <thomasphao04@gmail.com>
2024-10-10 11:51:15 -0700

    removed classname from motion element to stop fomring container

--- student-soc/src/pages/schedule.tsx
+++ student-soc/src/pages/schedule.tsx
@@ -12,7 +12,6 @@ const Schedule: React.FC = () => {
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
-          className="mt-8 p-6 bg-gray-800 text-white rounded-lg shadow-lg"
         >
           <h2 className="text-6xl font-bold text-blue-600 mt-8">
             Event Schedule
