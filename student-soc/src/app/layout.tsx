48883b2860fad6ad0cb05e6a693bc5b56906e7de
tommy phao <thomasphao04@gmail.com>
2024-10-09 20:02:02 -0700

    icon fix

--- student-soc/src/app/layout.tsx
+++ student-soc/src/app/layout.tsx
@@ -7,7 +7,7 @@ const Layout: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
     <div className="min-h-screen bg-gray-100 text-gray-900 flex flex-col">
       <header className="bg-black text-white p-4">
         <h1 className="text-2xl font-bold">Student Security Operations Center (SOC) @ Cal Poly Pomona</h1>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logo3.png" />
       </header>
       <main className="flex-grow p-4">{children}</main>
       <Footer />
