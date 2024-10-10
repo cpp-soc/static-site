941a01309bfcb4feb6b029c2376e759217685145
tommy phao <thomasphao04@gmail.com>
2024-10-10 11:43:07 -0700

    update

--- student-soc/src/data/schedule.ts
+++ student-soc/src/data/schedule.ts
@@ -1,9 +1,15 @@
-// src/data.js
-const eventData = [
+const eventData = {
+  dailySOCOperations: [
+    { day: 'Tuesday', time: '12:00pm - 6:00pm' },
+    { day: 'Wednesday', time: '12:00pm - 6:00pm' },
+    { day: 'Thursday', time: '12:00pm - 6:00pm' },
+  ],
+  botsEvent: [
     { day: 'Tuesday', time: '10:00am - 2:00pm' },
     { day: 'Wednesday', time: '10:00am - 2:00pm' },
     { day: 'Thursday', time: '10:00am - 2:00pm' },
     { day: 'Splunk BOTS training', time: '' },
-  ];
-  
-  export default eventData;
+  ],
+};
+
+export default eventData;
--- student-soc/src/pages/schedule.tsx
+++ student-soc/src/pages/schedule.tsx
@@ -8,8 +8,22 @@ const Schedule: React.FC = () => {
       <Navbar />
       <div className="container mx-auto p-4 min-h-screen flex flex-col items-center bg-transparent">
         <h2 className="text-3xl font-bold mb-4 gradient-text">Event Schedule</h2>
+        
+        <h3 className="text-2xl font-bold mb-2">Daily SOC Operations</h3>
+        <p className="mb-4">SOC Location: CLA Building 98 C5-15</p>
+        <ul className="w-full max-w-md bg-gray-800 text-white rounded-lg shadow-lg p-4 mb-8">
+          {eventData.dailySOCOperations.map((item, index) => (
+            <li key={index} className="flex justify-between py-2 border-b border-gray-700">
+              <span className="font-semibold">{item.day}</span>
+              <span>{item.time}</span>
+            </li>
+          ))}
+        </ul>
+
+        <h3 className="text-2xl font-bold mb-2">BOTS Event</h3>
+        <p className="mb-4">The training will be hosted on the following drop-in hours starting Oct 15 - Oct 30:</p>
         <ul className="w-full max-w-md bg-gray-800 text-white rounded-lg shadow-lg p-4">
-          {eventData.map((item, index) => (
+          {eventData.botsEvent.map((item, index) => (
             <li key={index} className="flex justify-between py-2 border-b border-gray-700">
               <span className="font-semibold">{item.day}</span>
               <span>{item.time}</span>
