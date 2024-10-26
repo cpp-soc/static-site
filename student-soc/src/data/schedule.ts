ed4977f5f2aa8c1c7dea1b9acc593bd8162939ec
xdkaine <55013938+xdkaine@users.noreply.github.com>
2024-10-26 15:46:31 -0700

    updated hours

--- student-soc/src/data/schedule.ts
+++ student-soc/src/data/schedule.ts
@@ -1,8 +1,9 @@
 const eventData = {
   dailySOCOperations: [
-    { day: 'Tuesday', time: '12:00pm - 6:00pm' },
-    { day: 'Wednesday', time: '12:00pm - 6:00pm' },
-    { day: 'Thursday', time: '12:00pm - 6:00pm' },
+    { day: 'Monday', time: '10:00pm - 3:00pm' },
+    { day: 'Tuesday', time: '10:00pm - 6:00pm' },
+    { day: 'Wednesday', time: '8:00pm - 6:00pm' },
+    { day: 'Thursday', time: '10:00pm - 6:00pm' },
   ],
   botsEvent: [
     { day: 'Tuesday', time: '10:00am - 2:00pm' },
