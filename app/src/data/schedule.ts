59efe91c4ba02624640168ab6b9e929e3197dbcb
Stickbug827 <ericheng23@gmail.com>
2026-01-24 18:03:46 -0800

    changing hours

--- app/src/data/schedule.ts
+++ app/src/data/schedule.ts
@@ -1,10 +1,10 @@
 const eventData = {
   dailySOCOperations: [
-    { day: 'Monday', time: '11:00 AM - 5:00 PM, 5:30 PM - 6:30 PM' },
+    { day: 'Monday', time: '11:00 AM - 2:00 PM, 4:00 PM - 5:00 PM' },
     { day: 'Tuesday', time: '10:00 AM - 5:00 PM' },
-    { day: 'Wednesday', time: '11:30 AM - 6:30 PM' },
-    { day: 'Thursday', time: '10:00 AM - 5:00 PM' },
-    { day: 'Friday', time: '1:30 PM - 5:00 PM' },
+    { day: 'Wednesday', time: '10:00 AM - 2:00 PM' },
+    { day: 'Thursday', time: '10:00 AM - 5:30 PM' },
+    
   ],
 /*   botsEvent: [
     { day: 'Tuesday', time: '10:00am - 2:00pm' },
