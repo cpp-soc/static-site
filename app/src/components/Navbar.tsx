fc1fb7db9501ab4cba5619e6dec5c58b269a2dd1
Sspirit <120894787+JoshRJHung@users.noreply.github.com>
2026-07-07 14:58:27 -0700

    Add Access link to Navbar component

--- app/src/components/Navbar.tsx
+++ app/src/components/Navbar.tsx
@@ -44,6 +44,12 @@ const Navbar: React.FC = () => {
             </svg>
             <span>Home</span>
           </Link>
+          <Link href="/access" className="text-white hover:bg-gray-700 px-4 py-2 rounded-lg transition duration-300 font-medium flex items-center space-x-2">
+            <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 11V7a4 4 0 018 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z"></path>
+            </svg>
+            <span>Access</span>
+          </Link>
           <Link href="/projects" className="text-white hover:bg-gray-700 px-4 py-2 rounded-lg transition duration-300 font-medium flex items-center space-x-2">
             <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10"></path>
