efbadccce22348f0903e3bb9cddd734c908608d2
Stickbug827 <ericheng23@gmail.com>
2026-08-20 22:45:09 -0700

    Fall 2026 update

--- app/src/data/schedule.ts
+++ app/src/data/schedule.ts
@@ -1,10 +1,10 @@
 const eventData = {
   dailySOCOperations: [
-    { day: 'Monday', time: '11:00 AM - 2:00 PM, 4:00 PM - 5:00 PM' },
-    { day: 'Tuesday', time: '10:00 AM - 5:00 PM' },
-    { day: 'Wednesday', time: '10:00 AM - 2:00 PM' },
+    { day: 'Monday', time: '10:00 AM - 5:30 PM' },
+    { day: 'Tuesday', time: '10:00 AM - 3:00 PM' },
+    { day: 'Wednesday', time: '10:00 AM - 5:30 PM' },
     { day: 'Thursday', time: '10:00 AM - 5:30 PM' },
-    
+    { day: 'Friday', time: '10:00 AM - 5:00 PM' }
   ],
 /*   botsEvent: [
     { day: 'Tuesday', time: '10:00am - 2:00pm' },
