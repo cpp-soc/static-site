517d5b11f271234d806cfdad64dc4b144557e42b
Stickbug827 <ericheng23@gmail.com>
2026-08-24 16:58:34 -0700

    Reducing my hours by a bit - Eric

--- app/src/data/schedule.ts
+++ app/src/data/schedule.ts
@@ -1,9 +1,9 @@
 const eventData = {
   dailySOCOperations: [
-    { day: 'Monday', time: '10:00 AM - 5:30 PM' },
+    { day: 'Monday', time: '10:00 AM - 5:00 PM' },
     { day: 'Tuesday', time: '10:00 AM - 3:00 PM' },
-    { day: 'Wednesday', time: '10:00 AM - 5:30 PM' },
-    { day: 'Thursday', time: '10:00 AM - 5:30 PM' },
+    { day: 'Wednesday', time: '10:00 AM - 5:00 PM' },
+    { day: 'Thursday', time: '10:00 AM - 5:00 PM' },
     { day: 'Friday', time: '10:00 AM - 5:00 PM' }
   ],
 /*   botsEvent: [
