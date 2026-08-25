8e4aadca285a67d7626146f12a0b449119274853
Stickbug827 <ericheng23@gmail.com>
2026-08-24 17:00:37 -0700

    Made a mistake on Thursday's hours, correcting

--- app/src/data/schedule.ts
+++ app/src/data/schedule.ts
@@ -3,7 +3,7 @@ const eventData = {
     { day: 'Monday', time: '10:00 AM - 5:00 PM' },
     { day: 'Tuesday', time: '10:00 AM - 3:00 PM' },
     { day: 'Wednesday', time: '10:00 AM - 5:00 PM' },
-    { day: 'Thursday', time: '10:00 AM - 5:00 PM' },
+    { day: 'Thursday', time: '10:00 AM - 3:00 PM' },
     { day: 'Friday', time: '10:00 AM - 5:00 PM' }
   ],
 /*   botsEvent: [
